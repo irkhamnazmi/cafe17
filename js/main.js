bfa17b74e0a6f26299c6c234f509c1c8d8ea375d
Irkham Nazmi <irkham.nazmi@gmail.com>
2021-05-27 11:44:35 +0700

    update page

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('#signup').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    modal = $(this)
+   
+    //  modal.find('.modal-title').text(recipient)
+})
+
+function login() {
+    document.getElementById("signup").style.display = "none"
+    document.getElementById("login").style.display = "block"
+  }
+
+  function signup() {
+    document.getElementById("login").style.display = "none"
+    document.getElementById("signup").style.display = "block"
+  }
+
+
+
+
+
