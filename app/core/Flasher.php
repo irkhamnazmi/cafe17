e489a96380c7dd1aefe9d085f5cf0ef5c374d3b2
irkhamnazmi <irkham.nazmi@gmail.com>
2022-01-28 13:23:16 +0700

    Update Terbaru

--- app/core/Flasher.php
+++ app/core/Flasher.php
@@ -16,8 +16,8 @@ class Flasher
     {
 
         if(isset($_SESSION['flash_cafe17'])){
-            echo '<div class="alert alert-' . $_SESSION['flash']['tipe'] . ' alert-dismissible fade show" role="alert">
-            <strong>' . $_SESSION['flash']['pesan'] . '</strong> ' . $_SESSION['flash']['aksi'] . '
+            echo '<div class="alert alert-' . $_SESSION['flash_cafe17']['tipe'] . ' alert-dismissible fade show" role="alert">
+            <strong>' . $_SESSION['flash_cafe17']['pesan'] . '</strong> ' . $_SESSION['flash_cafe17']['aksi'] . '
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
