13d8e8e8b51a3b1ad38c0a3ded5c77e0bd46b2b0
irkhamnazmi <irkham.nazmi@gmail.com>
2022-03-24 10:52:33 +0700

    Update About content

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,32 +1,27 @@
 <main>
-        <section class="herobwa mt-5">
-            <div class="container">
-                <div class="row">
-                    <div class="col align-self-stretch">
-                        <img width="300px" src="images/cafe17.png" alt="">
-                    </div>
-                    <div class="col align-self-stretch">
-                        <h1 style="font-size: 40pt;">
-                            Tentang Kami
-                        </h1>
-                        <p style="text-align: justify;">
-                            <span style="display:inline-block; width: 50px;"></span>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat ut lectus quis elementum
-                            blandit et. Ultrices aliquam est et sed ac vitae nunc. Vitae nibh hendrerit interdum nisl
-                            sagittis urna risus. Mattis etiam mauris erat adipiscing bibendum amet, neque, consectetur.
-                            Est velit hac bibendum semper et purus ut. Non id lobortis aliquet mattis eget posuere
-                            ornare. Morbi maecenas semper auctor sed in scelerisque. Massa egestas etiam dignissim
-                            cursus vitae faucibus id senectus. At nunc lacus viverra laoreet volutpat magna. Pulvinar
-                            odio tellus a ut id amet. Diam tempus luctus nunc tellus. Porttitor pellentesque bibendum
-                            non arcu vel venenatis tempor lectus orci. Nulla massa viverra adipiscing mattis egestas
-                            egestas condimentum felis. Ac volutpat neque, et diam praesent odio id massa vitae.
-                        </p>
-                        <!-- <a class="btn btn-primary" href="#" role="button">Get Started</a> -->
-                    </div>
+    <section class="herobwa mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col align-self-stretch">
+                    <img width="300px" src="images/cafe17.png" alt="">
+                </div>
+                <div class="col align-self-stretch">
+                    <h1 style="font-size: 40pt;">
+                        Tentang Kami
+                    </h1>
+                    <p style="text-align: justify;">
+                        <span style="display:inline-block; width: 50px;"></span>
+                        Rumah Makan Cafe 17 merupakan rumah makan yang menyajikan menu andalan mie ayam. Selain menu mie ayam, masih banyak lagi menu makanan yang rasanya enak sekali yaitu ayam penyet, telor penyet dan sop ayam kemangi. 
+                        Keanekaragaman menu dan jenis makanan juga terdiri dari keseimbangan dan kesadaran akan asupan nutrisi, yang tentunya dibalut dengan sentuhan citarasa tinggi. 
+                        Rumah Makan Cafe 17 juga memiliki desain interior yang unik, membuat dibeberapa sudut ruangan sering digunakan sebagai spot foto bagi para pelanggan yang datang. 
+                        Fasilitas internet gratis tanpa batas dengan kecepatan tinggi telah menjadikan Cafe 17 sebagai salah satu tempat paling favorit bagi anak muda dan kaum profesional lainnya.
+                    </p>
+                    <!-- <a class="btn btn-primary" href="#" role="button">Get Started</a> -->
                 </div>
             </div>
-            <br />
-            <br />
-        </section>
+        </div>
+        <br />
+        <br />
+    </section>
 
-    </main>
+</main>
